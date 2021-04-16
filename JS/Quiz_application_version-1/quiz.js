cfb79fd38ef8c0b519aa7fe2ee1cbae541c64436
satyajonnakuti <satyajonnakuti@msitprogran.net>
2021-04-16 10:42:48 +0530

    Quiz application with HTML,CSS,Bootstrap and JS

---
+++ JS/Quiz_application_version-1/quiz.js
@@ -0,0 +1,178 @@
+var username;
+var rollnum;
+var score = 0;
+var attempt=0;
+var displayname;
+function saveLogin()
+ { 
+    username = document.getElementById("username").value;
+    rollnum = document.getElementById("rollnum").value;
+    localStorage.setItem("username", username)
+    localStorage.setItem("rollnum", rollnum)
+    localStorage.setItem("score", score)
+    window.location.href = "q1.html"
+
+}
+function nextPage(qid, name, type, page)
+{
+    if(type == "radio" && qid == "q1"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if(ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+                // setScore(Number(getAttempt()) + 1)
+            }
+        }
+    }
+
+    if(type == "radio" && qid == "q2"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+            }
+        }
+    }
+
+    if(type == "radio" && qid == "q3"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+
+            }
+        }
+    }
+    if(type == "radio" && qid == "q4"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+
+            }
+        }
+    }
+    if(type == "radio" && qid == "q5"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+
+            }
+        }
+    }
+    if(type == "radio" && qid == "q6"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+
+            }
+        }
+    }
+    if(type == "radio" && qid == "q7"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+
+            }
+        }
+    }
+    if(type == "radio" && qid == "q8"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+                // setScore(Number(getAttempt()) + 1)
+            }
+        }
+    }
+    if(type == "radio" && qid == "q9"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+                // setScore(Number(getAttempt()) + 1)
+            }
+        }
+    }
+    if(type == "radio" && qid == "q10"){
+        ql = document.getElementsByName(name)
+        for(let i = 0; i < ql.length; i++)
+        {
+            console.log(ql[i].value)
+            if(ql[i].checked){attempt++}
+            if (ql[i].checked && ql[i].value == '100')
+            {
+                score = Number(getScore()) + 1
+                setScore(score)
+                // setScore(Number(getAttempt()) + 1)
+            }
+        }
+    }
+
+    window.location.href = page;
+}
+
+function getScore(){
+    return localStorage.getItem("score", score)
+}
+
+function setScore(score){
+    localStorage.setItem("score", score)
+}
+
+displayname = localStorage.getItem("username")
+displayroll = localStorage.getItem("rollnum")
+displayscore = localStorage.getItem("score")
+console.log(displayname, score, displayroll)
+document.getElementById("setUname").innerHTML = displayname + " - "+displayroll
+document.getElementById("result").innerHTML="Score: "+ displayscore
+// document.getElementById("attempted").innerHTML="Attempted: "+ attempt
+// document.getElementById("result").innerHTML = "Score: "+displayscore+"/3"
